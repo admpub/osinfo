53d6f8f471840b6cc081c59319af550b821825bb
Hank Shen <swh@admpub.com>
2023-07-07 23:26:09 +0800

    improved

--- ver_linux.go
+++ ver_linux.go
@@ -13,13 +13,56 @@ var (
 	verField   = regexp.MustCompile(`VERSION_ID=(.*?)\n|\nVERSION_ID=(.*?)\n`)
 )
 
+type LinuxMatcher struct {
+	MatchRegexp *regexp.Regexp
+	Distro      string
+	PkgManager  string
+	Version     string
+}
+
+var linuxMatchers = []LinuxMatcher{suse, debian, rhl, freebsd, arch, alpine, openwrt}
+
+func RegisterLinuxMatcher(m LinuxMatcher) {
+	linuxMatchers = append(linuxMatchers, m)
+}
+
 var (
-	suse    = regexp.MustCompile(`SLES|openSUSE`)
-	debian  = regexp.MustCompile(`Debian|Ubuntu|Kali|Parrot|Mint`)
-	rhl     = regexp.MustCompile(`Red Hat|CentOS|Fedora|Oracle`)
-	arch    = regexp.MustCompile(`Arch|Manjaro`)
-	alpine  = regexp.MustCompile(`Alpine`)
-	openwrt = regexp.MustCompile(`OpenWrt`)
+	suse = LinuxMatcher{
+		MatchRegexp: regexp.MustCompile(`SLES|openSUSE`),
+		Distro:      `opensuse`,
+		PkgManager:  `zypper`,
+	}
+	debian = LinuxMatcher{
+		MatchRegexp: regexp.MustCompile(`Debian|Ubuntu|Kali|Parrot|Mint`),
+		Distro:      `debian`,
+		PkgManager:  `apt`,
+	}
+	rhl = LinuxMatcher{
+		MatchRegexp: regexp.MustCompile(`Red Hat|CentOS|Fedora|Oracle`),
+		Distro:      `fedora`,
+		PkgManager:  `yum`,
+	}
+	freebsd = LinuxMatcher{
+		MatchRegexp: regexp.MustCompile(`FreeBSD`),
+		Distro:      `freebsd`,
+		PkgManager:  `pkg`,
+	}
+	arch = LinuxMatcher{
+		MatchRegexp: regexp.MustCompile(`Arch|Manjaro`),
+		Distro:      `arch`,
+		PkgManager:  `pacman`,
+		Version:     `rolling`,
+	}
+	alpine = LinuxMatcher{
+		MatchRegexp: regexp.MustCompile(`Alpine`),
+		Distro:      `alpine`,
+		PkgManager:  `apk`,
+	}
+	openwrt = LinuxMatcher{
+		MatchRegexp: regexp.MustCompile(`OpenWrt`),
+		Distro:      `openwrt`,
+		PkgManager:  `opkg`,
+	}
 )
 
 // GetVersion Linux returns version info
@@ -78,32 +121,15 @@ func GetVersion() Release {
 		return info
 	}
 
-	switch {
-	case suse.MatchString(name):
-		info.Linux.Distro = "opensuse"
-		info.Linux.PkgManager = "zypper"
-
-	case debian.MatchString(name):
-		info.Linux.Distro = "debian"
-		info.Linux.PkgManager = "apt"
-
-	case rhl.MatchString(name):
-		info.Linux.Distro = "fedora"
-		info.Linux.PkgManager = "yum"
-
-	case arch.MatchString(name):
-		info.Version = "rolling"
-		info.Linux.Distro = "arch"
-		info.Linux.PkgManager = "pacman"
-
-	case alpine.MatchString(name):
-		info.Linux.Distro = "alpine"
-		info.Linux.PkgManager = "apk"
-
-	case openwrt.MatchString(name):
-		info.Linux.Distro = "openwrt"
-		info.Linux.PkgManager = "opkg"
+	for _, m := range linuxMatchers {
+		if m.MatchRegexp.MatchString(name) {
+			info.Linux.Distro = m.Distro
+			info.Linux.PkgManager = m.PkgManager
+			if len(m.Version) > 0 {
+				info.Version = m.Version
+			}
+			return info
+		}
 	}
-
 	return info
 }
